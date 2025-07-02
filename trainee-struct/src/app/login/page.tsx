a1f2956173c2d68a40fd60599a27a3392b24e3a2
Vinícius Henrique <viniciushez@gmail.com>
2025-07-02 03:47:20 -0300

    interatividade com links e botões criada

--- trainee-struct/src/app/login/page.tsx
+++ trainee-struct/src/app/login/page.tsx
@@ -17,7 +17,7 @@ export default function Login() {
 
                     {/* Campo de email */}
                     <div className="flex flex-col w-full gap-2">
-                        <label htmlFor="email" className="text-[0.9rem] font-[700]">Email</label>
+                        <label htmlFor="email" className="text-[0.9rem] font-[700] text-gray-800">Email</label>
                         <div className="relative">
                             <span className="absolute left-3 top-1/2 -translate-y-1/2"><EmailIcon className="text-gray-400"/></span>
 
@@ -27,20 +27,21 @@ export default function Login() {
 
                     {/* Campo de senha */}
                     <div className="flex flex-col w-full gap-2">
-                        <label htmlFor="password" className="text-[0.9rem] font-[700]">Senha</label>
+                        <label htmlFor="password" className="text-[0.9rem] font-[700] text-gray-800">Senha</label>
                         <div className="relative">
                             <span className="absolute left-3 top-1/2 -translate-y-1/2"><PasswordIcon className="text-gray-400"/></span>
 
                             <input type={seePassword ? "text" : "password"} id="password" required placeholder="Digite sua senha" className="w-full border border-gray-300 rounded-lg text-gray-600 text-sm pl-10 pr-4 py-3 focus:outline-none focus:ring-1 focus:ring-gray-200" />
 
+                            {/* Botão e lógica com useState e useEffect para exibir senha */}
                             <button type="button" onClick={() => setSeePassword(!seePassword)} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer">
-                                {seePassword ? <OpenEyeIcon className="text-gray-400 hover:text-gray-800"/> : <ClosedEyeIcon className="text-gray-400 hover:text-gray-800"/>}
+                                {seePassword ? <OpenEyeIcon className="text-gray-400 hover:text-gray-800 transition-all duration-150 ease-in-out"/> : <ClosedEyeIcon className="text-gray-400 hover:text-gray-800 transition-all duration-150 ease-in-out"/>}
                             </button>
 
                         </div>
                     </div>
                     
-                    <button type="submit" className="w-full py-3 mt-8 bg-gradient-to-r from-[#DDA0DD] to-[#B8E6FF] text-[#5A5C8F] font-medium rounded-xl shadow-sm cursor-pointer">Entrar</button>
+                    <button type="submit" className="w-full py-3 mt-8 bg-gradient-to-r from-[#DDA0DD] to-[#B8E6FF] text-[#5A5C8F] font-medium rounded-xl shadow-sm cursor-pointer hover:text-[#696a9a] active:scale-[0.97] transition-transform duration-75 ease-in-out">Entrar</button>
                 </form>
 
                 <div className="flex items-center justify-center gap-4 w-full max-w-sm my-5">
@@ -49,12 +50,12 @@ export default function Login() {
                     <hr className="grow border-t-gray-300" />
                 </div>
 
-                <button type="button" className="flex justify-center items-center border border-gray-300 rounded-xl w-full max-w-sm max-h-[48px] py-3 text-sm font-bold cursor-pointer">
+                <button type="button" className="flex justify-center items-center border border-gray-300 rounded-xl w-full max-w-sm max-h-[48px] py-3 text-sm text-gray-800 font-bold cursor-pointer hover:text-[#696a9a] active:scale-[0.97] transition-transform duration-75 ease-in-out">
                     <GoogleIcon className="mr-2"/> Continuar com Google
                 </button>
 
-                <p className="mt-6 text-gray-600">Não tem uma conta? <a href="#" className="text-[#9333EA] text-[0.95rem] font-[600]">Cadastre-se</a></p>
-            </section>
+                <p className="mt-6 text-gray-600">Não tem uma conta? <a href="#" className="text-[#9333EA] text-[0.95rem] font-[600] hover:text-[#52118e]">Cadastre-se</a></p>
+            </section>   
         </main>
         </>
     );
