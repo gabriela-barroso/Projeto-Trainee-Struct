2714cfa36233b918f1c2c079f562d276f70b1158
Luisserra99 <luisserra99@gmail.com>
2025-07-27 13:09:56 -0300

    Ajustes nos icons

--- trainee-struct/src/app/checkout/page.tsx
+++ trainee-struct/src/app/checkout/page.tsx
@@ -1,20 +1,8 @@
 "use client";
 
 import { api } from "@/trpc/react";
+import { TrashIcon, MinusIcon, PlusIcon, ReturnIcon } from "../_components/icons";
 
-
-const Icon = ({ path, className = 'w-6 h-6', strokeWidth = 1.5 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={strokeWidth}
-    stroke="currentColor"
-    className={className}
-  >
-    <path strokeLinecap="round" strokeLinejoin="round" d={path} />
-  </svg>
-);
 
 // Define the type for a item in the cart
 interface CartItem {
@@ -57,19 +45,19 @@ const ProductItem = ({ item, onUpdateQuantity, onRemove }: {
           className="p-1.5 text-gray-700 hover:bg-pink-100 rounded-l-md disabled:opacity-50"
           disabled={item.quantity <= 1} // Disable the button if quantity is 1
         >
-          <Icon path="M18 12H6" className="w-4 h-4" />
+          <MinusIcon className="w-4 h-4" />
         </button>
         <span className="px-3 py-1 bg-blue-50 text-sm font-semibold text-gray-800">{item.quantity}</span>
         <button
           onClick={() => onUpdateQuantity(item.id, item.quantity + 1)}
           className="p-1.5 text-gray-700 hover:bg-pink-100 rounded-r-md"
         >
-          <Icon path="M12 6v12m-6-6h12" className="w-4 h-4" />
+          <PlusIcon className="w-4 h-4" />
         </button>
       </div>
       {/* Remove from cart button */}
       <button onClick={() => onRemove(item.id)} className="p-2 text-red-500 hover:text-red-700 hover:bg-red-100 rounded-full">
-        <Icon path="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12.578 0h11.218c.622 0 1.148.448 1.253 1.07l.164 1.155" className="w-5 h-5" />
+        <TrashIcon className="w-6 h-6"/>
       </button>
     </div>
   </div>
@@ -133,7 +121,7 @@ export default function CheckoutPage() {
         {/* Header Section */}
         <header className="flex items-center justify-between p-4 border-b border-gray-200 bg-white">
           <a href="/" className="flex items-center gap-2 px-4 py-2 text-sm font-semibold text-gray-700 bg-gradient-to-r from-purple-300 to-blue-300 rounded-md hover:opacity-90">
-            <Icon path="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" className="w-5 h-5" />
+            <ReturnIcon  className="w-5 h-5" />
             Voltar
           </a>
           <div className="flex items-center gap-2">
